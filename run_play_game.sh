5555720dda9f5c9c5ae1bcd71cc13515cec83692
moailemen <mo.ailemen@gmail.com>
2026-02-27 22:38:31 +0000

    Update run_play_game.sh

--- run_play_game.sh
+++ run_play_game.sh
@@ -4,4 +4,4 @@ apt install python3 python3-pip python3-venv -y
 python3 -m venv venv-game
 source venv-game/bin/activate
 pip install streamlit
-streamlit run guessing_game play_game2.py
+streamlit run guessing_game/play_game2.py
