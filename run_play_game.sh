3601babdacd7bb834c1f71dfdb69d8f10da51da6
moailemen <mo.ailemen@gmail.com>
2026-02-27 22:35:09 +0000

    Create run_play_game.sh

---
+++ run_play_game.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+apt install python3 python3-pip python3-venv -y
+python3 -m venv venv-game
+source venv-game/bin/activate
+pip install streamlit
+streamlit run guessing_game play_game2.py
